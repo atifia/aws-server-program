f34209eef3b4c0830d45b550fdc47087e80d9676
atifia <atefe_maleki@hotmail.com>
2016-09-13 20:09:28 +0200

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 
 from socket import *
 s = socket(AF_INET,SOCK_STREAM)
-s.connect(("www.python.org",80)) # Connect
+s.connect(("",4032)) # Connect
 s.send("GET /index.html HTTP/1.0\n\n") # Send request
 data = s.recv(10000) # Get response
 s.close()
